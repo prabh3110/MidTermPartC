061e020e9fa175a8a1f21d5afb6d2da395fe2b34
Prabal Manchanda <manchandaprabal878@gmial.com>
2023-03-08 16:01:58 -0500

    change genes to public

--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -18,13 +18,16 @@ public class UserProfile
     
     private String userID;//the userID
     private String genre;// the user's preferred genre of movie
-    private String[] genres = {"Comedy", "Drama", "Action", "Mystery"};//available genres of movies
+    public String[] genres = {"Comedy", "Drama", "Action", "Mystery"};//available genres of movies
     
     /**
      * A constructor that takes in the userID and the favourite genre
      * @param givenID the ID to assign to this user
      * @param givenGenre the users favourite genre
      */
+    UserProfile(){
+        
+    }
     public UserProfile(String givenID, String givenGenre)
     {
         userID = givenID;
